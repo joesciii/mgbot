2f8a7a94e09347836aaa3844d08426e4e551ca88
Joseph Scibetta <joseph.scibetta@ramboll.co.uk>
2020-07-16 12:55:11 +0100

    added basic bot skeleton

--- mgbot/Program.cs
+++ mgbot/Program.cs
@@ -3,13 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
 
 namespace mgbot
 {
     class Program
     {
         static void Main(string[] args)
+                  => new Program().StartAsync().GetAwaiter();
+        
+        
+        // bot named _client
+        private DiscordSocketClient _client;
+
+
+
+        public async Task StartAsync()
         {
+            _client = new DiscordSocketClient();
+
+            await _client.LoginAsync(TokenType.Bot, "discord bot token");
+
+            await _client.StartAsync();
+
+            // hook onto ready event
+            _client.Ready += Ready;
+
+
+            async Task Ready()
+            {
+
+                //run command module?
+                //do stuff in here
+
+            }
         }
     }
 }
+
